e3b275de675fa3879a14caf99caf214ea325eaac
Jonaires777 <airesjonathan123@gmail.com>
2024-12-03 17:24:39 -0300

    fix: fix merge conflicts

--- src/service.c
+++ src/service.c
@@ -64,126 +64,75 @@ void servicer(Darray *clients_array, pid_t analyst_pid) {
   }
 }
 
-void reception(int n_clients, Darray *queue) {
+void create_client(int x_time, Darray *queue) {
+  pid_t client_pid = fork();
+  if (client_pid == 0) {
+    execv("./client.c", "./client.c");
+    perror("client");
+    exit(EXIT_FAILURE);
+  } else {
+    Client *client = malloc(sizeof(Client));
+    client->pid = client_pid;
+    client->t_coming = time(NULL);
+
+    int demanda_fd = open("demanda.txt", O_RDONLY);
+    if (demanda_fd == -1) {
+      perror("client");
+      exit(EXIT_FAILURE);
+    }
+
+    int t_service;
+    if (read(demanda_fd, &t_service, sizeof(t_service)) != sizeof(t_service)) {
+      perror("client");
+      close(demanda_fd);
+      exit(EXIT_FAILURE);
+    }
+
+    close(demanda_fd);
+    remove("demanda.txt");
+    client->t_service = t_service;
+
+    srand(time(NULL));
+    if (rand() % 2) {
+      client->priority = x_time;
+    } else {
+      client->priority = x_time / 2;
+    }
+
+    ordered_insert(client, queue);
+  }
+}
+
+void reception(int n_clients, int x_time, Darray *queue) {
   sem_t *sem1, *sem2;
   if ((sem1 = sem_open("/sem_atend", O_CREAT)) == SEM_FAILED) {
     perror("sem_open failure");
     exit(EXIT_FAILURE);
   }
-  if ((sem2 = sem_open("/sem_block", O_CREAT)) == SEM_FAILED) {
+
+  if ((sem2 == sem_open("/sem_block", O_CREAT)) == SEM_FAILED) {
     perror("sem_open failure");
     exit(EXIT_FAILURE);
   }
 
-  if (n_clients == 0) {
+  for (int i = 0; i < n_clients; i++) {
+    create_client(x_time, queue);
   }
 
-  for (int i = 0; i < n_clients; ++i) {
-    pid_t client_pid = fork();
-    if (client_pid == 0) {
-      execv("./client.c", "./client.c");
-      perror("client");
-      exit(EXIT_FAILURE);
-    } else {
-      Client *client = malloc(sizeof(Client));
-      client->pid = client_pid;
-      client->t_coming = time(NULL);
-
-      int demanda_fd = open("demanda.txt", O_RDONLY);
-      if (demanda_fd == -1) {
-        perror("client");
-
-        void create_client(int x_time, Darray *queue) {
-          pid_t client_pid = fork();
-          if (client_pid == 0) {
-            execv("./client.c", "./client.c");
-            perror("client");
-            exit(EXIT_FAILURE);
-          } else {
-            Client *client = malloc(sizeof(Client));
-            client->pid = client_pid;
-            client->t_coming = time(NULL);
-
-            int demanda_fd = open("demanda.txt", O_RDONLY);
-            if (demanda_fd == -1) {
-              perror("client");
-              exit(EXIT_FAILURE);
-            }
-
-            int t_service;
-            if (read(demanda_fd, &t_service, sizeof(t_service)) !=
-                sizeof(t_service)) {
-              perror("client");
-              close(demanda_fd);
-              exit(EXIT_FAILURE);
-            }
-
-            close(demanda_fd);
-            remove("demanda.txt");
-            client->t_service = t_service;
-
-            srand(time(NULL));
-            if (rand() % 2) {
-              client->priority = x_time;
-            } else {
-              client->priority = x_time / 2;
-            }
-
-            ordered_insert(client, queue);
-          }
-        }
-
-        void reception(int n_clients, int x_time, Darray *queue) {
-          sem_t *sem1, *sem2;
-          if ((sem1 = sem_open("/sem_atend", O_CREAT)) == SEM_FAILED) {
-            perror("sem_open failure");
-            exit(EXIT_FAILURE);
-          }
-
-          int time;
-          if (read(demanda_fd, &time, sizeof(time)) != sizeof(time)) {
-            perror("client");
-            close(demanda_fd);
-            exit(EXIT_FAILURE);
-          }
-
-          close(demanda_fd);
-          remove("demanda.txt");
-          client->t_service = time;
-          darray_push_back(queue, client);
-        }
-      }
-    }
-
-    int main(int argc, char **argv) {
-      if (argc < 3) exit(EXIT_FAILURE);
-
-      int n_clients = atoi(argv[1]);
-      int time = atoi(argv[2]);
-      Darray *clients_queue = darray_init(n_clients, NULL);
-
-      darray_free(clients_queue);
-      return EXIT_SUCCESS;
-    }
-
-    for (int i = 0; i < n_clients; ++i) {
+  if (n_clients == 0) {
+    while (1) {
       create_client(x_time, queue);
     }
-
-    if (n_clients == 0) {
-      while (1) {
-        create_client(x_time, queue);
-      }
-    }
   }
+}
 
-  int main(int argc, char **argv) {
-    if (argc < 3) exit(EXIT_FAILURE);
+int main(int argc, char **argv) {
+  if (argc < 3) exit(EXIT_FAILURE);
 
-    int n_clients = atoi(argv[1]);
-    int time = atoi(argv[2]);
-    Darray *clients_queue = darray_init(n_clients, NULL);
+  int n_clients = atoi(argv[1]);
+  int time = atoi(argv[2]);
+  Darray *clients_queue = darray_init(n_clients, NULL);
 
-    darray_free(clients_queue);
-    return EXIT_SUCCESS;
-  }
+  darray_free(clients_queue);
+  return EXIT_SUCCESS;
+}
