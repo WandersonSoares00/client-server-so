34fa71d99686cae7a3ae7dbc622251be76f24e1c
Wanderson Soares <wandersonsoares841@gmail.com>
2024-12-19 13:58:30 -0300

    Va lah

--- src/main.c
+++ src/main.c
@@ -8,14 +8,14 @@ int main(int argc, char **argv) {
 
     parse_arguments(argc, argv, &opts);
 
-    pthread_t reception_id, servicer_id;
+    pthread_t reception_id, servicer_id, input_id;
 
     ClientsQueue clients_queue;
-    clients_queue.data = darray_init(opts.num_clients, dealoc_client);
+    clients_queue.data = darray_init(1, dealoc_client);
+    //clients_queue.data = darray_init(opts.num_clients, dealoc_client);
 
     pthread_mutex_init(&clients_queue.mutex, NULL);
     pthread_cond_init(&clients_queue.not_empty, NULL);
-    pthread_cond_init(&clients_queue.not_full, NULL);
 
     pid_t analyst_pid = invoke_analyst(opts.no_analyst);
     
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
     }
     
     if (opts.num_clients == 0) {
-        if (pthread_create(&reception_id, NULL, reception, &reception_args) != 0) {
+        if (pthread_create(&input_id, NULL, input_thread, &reception_args) != 0) {
             perror("Failed to create input worker");
             exit(EXIT_FAILURE);
         }
@@ -48,6 +48,7 @@ int main(int argc, char **argv) {
 
     ServiceReturnValues *ret;
 
+    pthread_join(input_id, NULL);
     pthread_join(reception_id, NULL);
     reception_thread_done = 1;
     pthread_join(servicer_id, (void**)&ret);
@@ -58,7 +59,7 @@ int main(int argc, char **argv) {
 
     kill(analyst_pid, SIGKILL);
     
-    printf("%d clientes recebidos e %d atendidos\n", opts.num_clients == 0 ? ret->clients : opts.num_clients, opts.num_clients);
+    printf("%d clientes recebidos e %d atendidos\n", opts.num_clients == 0 ? ret->clients : opts.num_clients, ret->clients);
     printf("Taxa de satisfação: %d / %d = %.4f\n",  ret->clients_satisfied, ret->clients, (float) ret->clients_satisfied / ret->clients);
     printf("Tempo total de execução: %ld clocks(%f s)\n", ret->exec_time, (double) ret->exec_time / CLOCKS_PER_SEC);
     print_resource_statistics();
